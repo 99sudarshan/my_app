44b0c9f96a9cb588e6b902a6551848316ae796ea
99sudarshan <91446640+99sudarshan@users.noreply.github.com>
2021-11-29 15:57:03 +0545

    msg

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -29,10 +29,10 @@ export default function Navbar(props) {
             </form>           
           </div>
           <div className="d-flex">
-            <div className="bg-primary rounded mx-2 " onClick={()=>props.toggleMode('primary')} style={{height:'30px',width:'30px'}}></div>
-            <div className="bg-warning rounded mx-2 " onClick={()=>props.toggleMode('warning')} style={{height:'30px',width:'30px'}}></div>
-            <div className="bg-success rounded mx-2 " onClick={()=>props.toggleMode('success')} style={{height:'30px',width:'30px'}}></div>
-            <div className="bg-danger rounded mx-2 " onClick={()=>props.toggleMode('danger')} style={{height:'30px',width:'30px'}}></div>
+            <div className="bg-primary rounded mx-2 " onClick={()=>props.toggleMode('primary')} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+            <div className="bg-warning rounded mx-2 " onClick={()=>props.toggleMode('warning')} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+            <div className="bg-success rounded mx-2 " onClick={()=>props.toggleMode('success')} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+            <div className="bg-danger rounded mx-2 " onClick={()=>props.toggleMode('danger')} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
         </div>
           <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
               <input className="form-check-input" onClick={()=>props.toggleMode('null')} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
